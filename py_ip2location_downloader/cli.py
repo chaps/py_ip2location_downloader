b3d5ae946e8bd5e73699aa67e35407fb31d2e3a2
Chaps <drumchaps@gmail.com>
2020-09-09 20:40:20 -0500

    Feature adds cli util (#4)
    * Adds unzipper utility class
    
    * Adds base for CLI utility

--- py_ip2location_downloader/cli.py
+++ py_ip2location_downloader/cli.py
@@ -1,17 +1,57 @@
 """Console script for py_ip2location_downloader."""
 import argparse
 import sys
+from .unzipper import Unzipper
+from .downloader import download_free_db_zip, \
+    download_auth_db_zip
+import tempfile
+import os
+
+class CliUtil:
+    def __init__(
+        self, download_path=None, download_type="free", ipversion="ipv4", _format="CSV", 
+        unzip=True,
+        *args, **kwargs
+    ):
+        if download_path:
+            self.download_path = download_path
+        else:
+            self.download_path = os.path.join(tempfile.gettempdir(), "ip2location_db.zip")
+        
+        self.download_type = download_type
+        self.ipversion = ipversion
+        self.format = _format
+        self.unzip = unzip
+        
+
+
+    def exec(self, token=None):
+        if self.download_type == "free":
+            download_free_db_zip(self.download_path)
+        else:
+            token = token if token else os.environ.get("IP2LOCATION_AUTH_TOKEN")
+            download_auth_db_zip(TOKEN=token)
+        if self.unzip:
+            output_path = Unzipper(self.download_path).unzip()
+
+    pass
 
 
 def main():
     """Console script for py_ip2location_downloader."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
-    args = parser.parse_args()
+    parser.add_argument("--download_path", default=None,help="Free or auth")
+    parser.add_argument("--download_type", default="free",help="Free or auth")
+    parser.add_argument("--ipversion", default="ipv4", help="IP Version format ")
+    parser.add_argument("--format", default="csv", help="DB AVAILABLE FORMATS CSV or BIN")
+    parser.add_argument("--product", default="db1", help="PRODUCT")
+    parser.add_argument("--token", help="token used in order to authenticate in case of downloading the auth required DBs ")
+    parser.add_argument("--unzip", default=True, help="")
 
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "py_ip2location_downloader.cli.main")
+    args = parser.parse_args()
+    
+    cli_util = CliUtil( **{x:y for x, y in args._get_kwargs()})
+    cli_util.exec()
     return 0
 
 
--- py_ip2location_downloader/unzipper.py
+++ py_ip2location_downloader/unzipper.py
@@ -2,16 +2,14 @@
 
 import zipfile
 import os
-
-from .downloader import download_auth_db_zip, \
-    download_free_db_zip
+import tempfile
 
 
 class Unzipper:
     """
     """
     def __init__(self, target_zip):
-        self.target_zip = target_zip
+        self.target_zip = target_zip    
 
 
     def get_db_file_ref(self, zip_ref_namelist):
@@ -25,14 +23,20 @@ class Unzipper:
     def unzip(self, target_path=None, only_db=True, delete_zip=True):
         """
         """
+        # TODO: implement rename DB file: os.rename(src, dst)
         with zipfile.ZipFile(self.target_zip, 'r') as zip_ref:
-            
+            file_name_ref = self.get_db_file_ref(zip_ref.namelist())
             if only_db:
-                print(zip_ref.namelist())
-                file_name_ref = self.get_db_file_ref(zip_ref.namelist())
-
                 zip_ref.extract(file_name_ref, path=target_path)
             else:
                 zip_ref.extractall(path=target_path)
         if delete_zip:
             os.remove(self.target_zip)
+        if target_path:
+            output_path = os.path.join(target_path, file_name_ref)
+        else:
+            output_path = os.path.join(os.getcwd(), file_name_ref)
+        # LOG
+        print("DB unzipped and located at {}".format(output_path))    
+        
+        return output_path
