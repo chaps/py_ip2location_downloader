4879908ecedd762dab456385b306b355b0dc8b46
Chaps <drumchaps@gmail.com>
2020-09-10 09:12:50 -0500

    Adds linter to ci (#7)
    * Adds unzipper utility class
    
    * Adds base for CLI utility
    
    * Adds number to IP functionality
    
    * Adds credits for no2ip function
    
    * Adds travis-ci yaml file
    
    * Fix rename correct travis ci file name
    
    * Loads requirements file for install dependencies
    
    * Adds support for python 3.4
    
    * Adds support for python 3.4
    
    * Removes python 3.4 support
    
    * Remove python 3.4 from travis-ci targets
    
    * Removes wrong travis ci file
    
    * Adds flake8 to CI and fix flake8 lint issues
    
    * Fix lint issues

--- py_ip2location_downloader/cli.py
+++ py_ip2location_downloader/cli.py
@@ -8,29 +8,33 @@ from .number_to_ip import csv_number_to_ip
 import tempfile
 import os
 
+
 class CliUtil:
     def __init__(
-        self, download_path=None, download_type="free", ipversion="ipv4", _format="CSV", 
+        self,
+        download_path=None,
+        download_type="free", ipversion="ipv4", _format="CSV",
         unzip=True, numbertoipv4=True,
         *args, **kwargs
     ):
         if download_path:
             self.download_path = download_path
         else:
-            self.download_path = os.path.join(tempfile.gettempdir(), "ip2location_db.zip")
-        
+            self.download_path = os.path.join(
+                tempfile.gettempdir(), "ip2location_db.zip"
+            )
         self.download_type = download_type
         self.ipversion = ipversion
         self.format = _format
         self.unzip = unzip
         self.numbertoipv4 = numbertoipv4
 
-
     def exec(self, token=None):
         if self.download_type == "free":
             download_free_db_zip(self.download_path)
         else:
-            token = token if token else os.environ.get("IP2LOCATION_AUTH_TOKEN")
+            if not token:
+                token = os.environ.get("IP2LOCATION_AUTH_TOKEN")
             download_auth_db_zip(TOKEN=token)
         if self.unzip:
             output_path = Unzipper(self.download_path).unzip()
@@ -41,18 +45,37 @@ class CliUtil:
 def main():
     """Console script for py_ip2location_downloader."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--download_path", default=None,help="Free or auth")
-    parser.add_argument("--download_type", default="free",help="Free or auth")
-    parser.add_argument("--ipversion", default="ipv4", help="IP Version format ")
-    parser.add_argument("--format", default="csv", help="DB AVAILABLE FORMATS CSV or BIN")
+    parser.add_argument(
+        "--download_path",
+        default=None,
+        help="Free or auth"
+    )
+    parser.add_argument(
+        "--download_type", default="free", help="Free or auth"
+    )
+    parser.add_argument(
+        "--ipversion",
+        default="ipv4", help="IP Version format "
+    )
+    parser.add_argument(
+        "--format",
+        default="csv",
+        help="DB AVAILABLE FORMATS CSV or BIN"
+    )
     parser.add_argument("--product", default="db1", help="PRODUCT")
-    parser.add_argument("--token", help="token used in order to authenticate in case of downloading the auth required DBs ")
+    parser.add_argument(
+        "--token",
+        help="token used in order to authenticate"
+        "in case of downloading the auth required DBs"
+    )
     parser.add_argument("--unzip", default=True, help="")
     parser.add_argument("--numbertoipv4", default=True, help="")
 
     args = parser.parse_args()
-    
-    cli_util = CliUtil( **{x:y for x, y in args._get_kwargs()})
+
+    cli_util = CliUtil(
+        **{x: y for x, y in args._get_kwargs()}
+    )
     cli_util.exec()
     return 0
 
--- py_ip2location_downloader/downloader.py
+++ py_ip2location_downloader/downloader.py
@@ -1,13 +1,16 @@
 import requests
 from enum import Enum
 
+
 class IPVersionEnum(Enum):
     IPV4 = ""
     IPV6 = ".IPV6"
 
+
 class FormatEnum(Enum):
     CSV = "CSV"
     BIN = "BIN"
+
 
 class ProductCodeEnum(Enum):
     DB1 = "DB1"
@@ -15,19 +18,25 @@ class ProductCodeEnum(Enum):
     DB5 = "DB5"
     DB9 = "DB9"
     DB11 = "DB11"
-    
 
-URL_DOWNLOAD_DB_URL = "http://download.ip2location.com/lite/IP2LOCATION-LITE-DB1{IP_VERSION}.{FORMAT}.ZIP"
+
+URL_DOWNLOAD_DB_URL = "http://download.ip2location.com/lite/\
+IP2LOCATION-LITE-DB1{IP_VERSION}.{FORMAT}.ZIP"
 
 URL_AUTH_DOWNLOAD_DB_URL = "https://www.ip2location.com/download/"
 
 
-def download_free_db_zip(target, IP_VERSION=IPVersionEnum.IPV4, FORMAT=FormatEnum.CSV):
+def download_free_db_zip(
+    target, IP_VERSION=IPVersionEnum.IPV4, FORMAT=FormatEnum.CSV
+):
     """
     """
-    download_url = URL_DOWNLOAD_DB_URL.format(IP_VERSION=IP_VERSION.value, FORMAT=FORMAT.value)
+    download_url = URL_DOWNLOAD_DB_URL.format(
+        IP_VERSION=IP_VERSION.value,
+        FORMAT=FORMAT.value
+    )
     response = requests.get(download_url)
-    
+
     with open(target, "wb") as file_object:
         file_object.write(response.content)
     print("IP2LOCATION zip file Downloaded at path: {}".format(target))
@@ -37,8 +46,7 @@ def download_free_db_zip(target, IP_VERSION=IPVersionEnum.IPV4, FORMAT=FormatEnu
 def build_product_code_format(PRODUCT, IP_VERSION, FORMAT):
 
     format_value = "" if FORMAT == FormatEnum.CSV else FORMAT.value
-    ip_version =  "" if IP_VERSION == IPVersionEnum.IPV4 else IP_VERSION.value
-    
+    ip_version = "" if IP_VERSION == IPVersionEnum.IPV4 else IP_VERSION.value
     return "{product}LITE{format}{ip_version}".format(
         product=PRODUCT.value,
         format=format_value,
@@ -50,7 +58,7 @@ def download_auth_db_zip(target, TOKEN, PRODUCT, IP_VERSION, FORMAT):
     file_code = build_product_code_format(PRODUCT, IP_VERSION, FORMAT)
     response = requests.get(
         URL_AUTH_DOWNLOAD_DB_URL,
-        params={"token": TOKEN , "file": file_code},
+        params={"token": TOKEN, "file": file_code},
         stream=True
     )
     with open(target, "wb") as file_object:
